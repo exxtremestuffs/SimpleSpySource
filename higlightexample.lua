a8a10b701f79017087dc27bdbdeb207d3bc9f749
exx <superjamrs323@gmail.com>
2020-04-09 02:02:26 -0700

    updated documentation comments

--- higlightexample.lua
+++ higlightexample.lua
@@ -4,7 +4,7 @@ local myFrame = Instance.new("Frame")
 --- Instantiates and returns a new Highlight instance- creates the actual gui within the frame
 local myHighlight = Highlight.new(myFrame)
 
---- Sets the raw text of the code box (\n = new line, \t converted to spaces)
+--- Sets the raw text of the code box (\n = new line)
 myHighlight:setRaw(string)
 
 --- Returns the (string) raw text of the code box (\n = new line)
@@ -19,7 +19,7 @@ myHighlight:size()
 --- Returns the (string) line of the specified line number
 myHighlight:getLine(int)
 
---- Replaces the specified line number with the specified string (\n will overwrite further lines)
+--- Replaces the specified line number with the specified string (\n will insert further lines)
 myHighlight:setLine(int, string)
 
 --- Inserts a line made from the specified string and moves all existing lines down (\n will insert further lines)
