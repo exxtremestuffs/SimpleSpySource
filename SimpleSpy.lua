8f9cb007179e7c2bd7806ef76990e4204aeaaaec
exx <superjamrs323@gmail.com>
2020-04-16 17:40:20 -0700

    removed old codebox

--- SimpleSpy.lua
+++ SimpleSpy.lua
@@ -41,9 +41,7 @@ local UIListLayout_2 = Instance.new("UIListLayout")
 local functionTemplate = Instance.new("ImageButton")
 local name_3 = Instance.new("TextLabel")
 local description = Instance.new("TextLabel")
-local code = Instance.new("Frame")--"ScrollingFrame")
--- local codebox = Instance.new("TextBox")
--- local lines = Instance.new("TextLabel")
+local code = Instance.new("Frame")
 local codebox = Highlight.new(code)
 
 --Properties:
@@ -270,36 +268,6 @@ code.BackgroundColor3 = Color3.new(0.137255, 0.137255, 0.137255)
 code.BorderSizePixel = 0
 code.Size = UDim2.new(1, 0, 0.5, 0)
 code.ZIndex = 0
--- code.CanvasSize = UDim2.new(0, 0, 0, 0)
--- code.HorizontalScrollBarInset = Enum.ScrollBarInset.Always
--- code.ScrollBarThickness = 10
-
--- codebox.Name = "codebox"
--- codebox.Parent = code
--- codebox.BackgroundColor3 = Color3.new(1, 1, 1)
--- codebox.BackgroundTransparency = 1
--- codebox.Position = UDim2.new(0, 18, 0, 0)
--- codebox.Size = UDim2.new(1, -18, 1, 0)
--- codebox.Font = Enum.Font.SourceSans
--- codebox.Text = "-- hiya, you shouldn't be seeing this"
--- codebox.TextColor3 = Color3.new(0.87451, 0.87451, 0.87451)
--- codebox.TextSize = 14
--- codebox.TextXAlignment = Enum.TextXAlignment.Left
--- codebox.TextYAlignment = Enum.TextYAlignment.Top
--- codebox.ZIndex = 0
--- codebox.ClearTextOnFocus = false
-
--- lines.Name = "lines"
--- lines.Parent = code
--- lines.BackgroundColor3 = Color3.new(0.0823529, 0.0823529, 0.0823529)
--- lines.BorderSizePixel = 0
--- lines.Size = UDim2.new(0, 15, 1, 0)
--- lines.Font = Enum.Font.SourceSansSemibold
--- lines.Text = "1"
--- lines.TextColor3 = Color3.new(0.466667, 0.494118, 1)
--- lines.TextSize = 14
--- lines.TextYAlignment = Enum.TextYAlignment.Top
--- lines.ZIndex = 0
 
 -------------------------------------------------------------------------------
 -- init
@@ -521,53 +489,6 @@ function toggleSideTray(override)
         wait(0.5)
     end
     sideClosing = false
-end
-
---- Runs when the cursor position changes, saves the previous value and current value to variables
-function onCursorPosChange()
-    -- lastCursorPos = cursorPos
-    -- cursorPos = codebox.CursorPosition
-end
-
---- Returns a table of {number of lines: number, canvas size:UDim2} from the codebox
-function getLinesAndCanvas()
-    -- local lineNumber = 0
-    -- for _ in string.gmatch(codebox.Text, "\n") do
-    --     lineNumber = lineNumber + 1
-    -- end
-    -- local size = TextService:GetTextSize(codebox.Text, 14, Enum.Font.SourceSans, Vector2.new(math.huge, math.huge))
-    -- return {lineNumber, UDim2.new(0, size.X + 25, 0, size.Y)}
-end
-
---- Runs every time the codebox is modified
-function updateCodebox()
-    -- local lineNumber, canvasSize = unpack(getLinesAndCanvas())
-    -- code.CanvasSize = canvasSize
-    -- local lineString = ""
-    -- for i = 1, lineNumber + 1 do
-    --     lineString = lineString .. tostring(i) .. "\n"
-    -- end
-    -- lines.Text = lineString
-end
-
---- Used to have enter key create a new line
-function onDeselect(isEnter)
-    -- if isEnter and cursorPos == -1 then
-    --     local cPos = lastCursorPos
-    --     if cPos == #codebox.Text then
-    --         codebox.Text = codebox.Text .. "\n"
-    --         codebox.Text = codebox.Text:sub(1, #codebox.Text - 1)
-    --     elseif cPos == 0 then
-    --         codebox.Text = "\n" .. codebox.Text
-    --         codebox.Text = codebox.Text:sub(2, #codebox.Text)
-    --         codebox.Text = codebox.Text:sub(1, 1) .. codebox.Text:sub(3, #codebox.Text)
-    --     else
-    --         codebox.Text = codebox.Text:sub(1, cPos) .. "\n" .. codebox.Text:sub(cPos, #codebox.Text)
-    --         codebox.Text = codebox.Text:sub(1, cPos) .. codebox.Text:sub(cPos + 2, #codebox.Text)
-    --     end
-    --     codebox:CaptureFocus()
-    --     codebox.CursorPosition = cPos + 1
-    -- end
 end
 
 --- Gets the player an instance is descended from
