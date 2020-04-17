e2355428c3066745cf7d7ff61f58d8e189eaebb7
exx <superjamrs323@gmail.com>
2020-04-16 23:15:49 -0700

    updated link for merge

--- SimpleSpy.lua
+++ SimpleSpy.lua
@@ -13,7 +13,7 @@ end
 
 local Players = game:GetService("Players")
 local CoreGui = game:GetService("CoreGui")
-local Highlight = loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/SimpleSpy-Syntax-Highlighted/highlight.lua"))()
+local Highlight = loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/highlight.lua"))()
 
 ---- GENERATED (kinda sorta mostly) BY GUI to LUA ----
 
