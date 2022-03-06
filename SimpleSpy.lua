45b87b44b86f8f4b0ed06b4dc23a399e7e4f33f6
exx <superjamrs323@gmail.com>
2022-03-05 17:27:07 -0700

    changed highlight import to SP require

--- SimpleSpy.lua
+++ SimpleSpy.lua
@@ -15,10 +15,12 @@ end
 
 local Players = game:GetService("Players")
 local CoreGui = game:GetService("CoreGui")
-local Highlight =
-	loadstring(
-		game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/highlight.lua")
-	)()
+local SP =
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/SimplePackageManager/master/init.lua"))():setCurrentProject(
+		"exxtremestuffs/SimpleSpySource"
+	)
+
+local Highlight = SP:require("highlight")
 
 ---- GENERATED (kinda sorta mostly) BY GUI to LUA ----
 
