a773ec00e568bae7171bd70c6d4877e0ff89cf69
Paygammy <49263433+paygammy@users.noreply.github.com>
2022-08-29 20:42:23 -0700

    Update highlight-example.lua

--- highlight-example.lua
+++ highlight-example.lua
@@ -1,11 +1,11 @@
 type highlightModule = {
-	setRaw: (self: highlightModule, index: string) -> (),
-	getRaw: (self: highlightModule) -> string,
-	getTable: (self: highlightModule) -> {[number]: string},
-	getSize: (self: highlightModule) -> number,
-	getLine: (self: highlightModule, int: number) -> string, 
-	setLine: (self: highlightModule, int: number, string) -> number,
-	insertLine: (self: highlightModule, int: number, string) -> ()
+	setRaw: (self: highlightModule, index: string) -> (), -- # undocumented
+	getRaw: (self: highlightModule) -> string, -- # undocumented
+	getTable: (self: highlightModule) -> {[number]: string}, -- # get all existing lines
+	getSize: (self: highlightModule) -> number, -- # get list size
+	getLine: (self: highlightModule, int: number) -> string,  -- # get existing line by id
+	setLine: (self: highlightModule, int: number, string) -> number, -- # create/modify new line with existing/no behavior
+	insertLine: (self: highlightModule, int: number, string) -> () -- # create a new line with internal behavior
 }
 
 return require(...) :: highlightModule
