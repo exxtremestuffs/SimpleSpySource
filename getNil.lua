0a9c0bcfc00d72c0713dd79d9b66eda4a16fdadf
Paygammy <49263433+paygammy@users.noreply.github.com>
2022-08-29 20:26:15 -0700

    Update getNil.lua

--- getNil.lua
+++ getNil.lua
@@ -1,2 +1,7 @@
--- Used in script generator for getting nil instances
-function getNil(name,class) for _,v in pairs(getnilinstances())do if v.ClassName==class and v.Name==name then return v;end end end
+function getNil(name, class)
+    for _, v in pairs(getnilinstances()) do
+        if typeof(v) == 'Instance' and v.ClassName == class and v.Name == name then
+            return v
+        end
+    end
+end
